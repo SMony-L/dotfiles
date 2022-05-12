66f2d8dc4f2321a45a507062d41ff53dec67f3fd
SMony-L <l.sovanmony@gmail.com>
2022-05-12 17:37:47 -0400

    add brew.sh

---
+++ mac/brew.sh
@@ -0,0 +1,18 @@
+#!/usr/bin/env bash
+
+brew install telnet
+brew install certbot
+brew install ssh-agent
+brew install sslscan
+brew install wget
+brew install node
+brew install git
+brew install jq
+brew install htop
+brew install kubernetes-cli
+brew install maven
+brew install gradle
+brew install youtube-dl
+brew install zsh
+brew install zsh-completions
+brew install wireshark
